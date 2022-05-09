bb0da67bb0cd9877437aa963c09acb0e645fdc2e
RaeganJaeger <rjaeger4@gmail.com>
2022-05-08 18:42:14 -0700

    Created Dictionary Component with basic functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
+      <div className="container">
+        <h1>Dictionary App</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <p className="footer">
+          Opensource code by{" "}
+          <a
+            href="https://github.com/RaeganJaeger/weather-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Raegan Jaeger
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
