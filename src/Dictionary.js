ea95449a10a498c6781134dc22c6b322d9b9da6a
RaeganJaeger <rjaeger4@gmail.com>
2022-05-11 13:38:48 -0700

    Added photos component and made pexels api call

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import "./Dictionary.css";
@@ -9,18 +10,30 @@ export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     //console.log(response.data[0]);
     // the 0 represents only using the first definition. (remove 0 if I want multiple definitions)
     //console.log(response.data[0].meanings[0].definitions[0].definition);
     setResults(response.data[0]);
   }
 
+  function handlePexelsResponse(response) {
+    //console.log(response.data);
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     //documentation : https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000175fb5ff5e8904ab9a1864d281fe70c4e";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=5`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -52,6 +65,7 @@ export default function Dictionary(props) {
           </button>
         </form>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
